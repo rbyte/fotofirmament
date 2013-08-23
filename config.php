b0e292ba13dbdde98bd74c654aa246237f1fd3ef
matmax <matthias.graf@st.ovgu.de>
2013-08-23 13:29:14 +0200

    Revert "fotograf initial commit"
    This reverts commit 8571703f6de888917af5ad988ba1d1e0334139d2.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$pathToPhotos = "fotograf/long1920_q70/";
+$pathToPhotos = "images/photosFullsize/";
 // if there are no previews, you can set pathToPreviews = pathToPhotos
 //$pathToPreviews = $pathToPhotos;
 // preview and fullsize version have the same file name!
-$pathToPreviews = "fotograf/long300_q70/";
+$pathToPreviews = "images/photosPreview/";
 ?>
