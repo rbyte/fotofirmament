fd7f585b3c2e0335d325f2e73655e08502369b37
matmax <matthias.graf@st.ovgu.de>
2013-10-19 17:18:16 +0200

    dir name change

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $pathToPhotos = "fotograf/long1920_q70/";
 // if there are no previews, you can set pathToPreviews = pathToPhotos
 //$pathToPreviews = $pathToPhotos;
 // preview and fullsize version have the same file name!
-$pathToPreviews = "fotograf/long300_q70/";
+$pathToPreviews = "fotograf/previews/";
 
 // in case previews do not exist, they are generated
 $previewLongEdgeSizePX = 420;
